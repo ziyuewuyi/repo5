4284fcc3b5cae4914ed68adff24a688d3616448d
xubo <wojiaoxubo@yeah.net>
2019-01-22 14:31:25 +0800

    king

--- dosomething/src/main/java/test/Demo.java
+++ dosomething/src/main/java/test/Demo.java
@@ -5,5 +5,6 @@ public class Demo {
         System.out.println("first commmit");
         System.out.println("second commit");
         System.out.println("i am laiangnaduo dikapuliao");
+        System.out.println("猴戏不姓帐，望百花齐放");
     }
 }
