e4308917e6dff26fbf8f22ac6ae66c9b100b22ca
xubo <wojiaoxubo@yeah.net>
2019-01-22 14:19:13 +0800

    test1 update

--- dosomething/src/main/java/test/Demo.java
+++ dosomething/src/main/java/test/Demo.java
@@ -4,5 +4,6 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("first commmit");
         System.out.println("second commit");
+        System.out.println("i am laiangnaduo dikapuliao");
     }
 }
