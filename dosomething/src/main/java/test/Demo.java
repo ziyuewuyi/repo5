f34b9faf2ef161b14eff4d796eb301013e1fab9d
xubo <wojiaoxubo@yeah.net>
2019-01-22 15:42:57 +0800

    test1 update3

--- dosomething/src/main/java/test/Demo.java
+++ dosomething/src/main/java/test/Demo.java
@@ -5,7 +5,6 @@ public class Demo {
         System.out.println("first commmit");
         System.out.println("second commit");
         System.out.println("i am laiangnaduo dikapuliao");
-
         System.out.println("猴戏不姓帐，望百花齐放");
         System.out.println("文体两开花");
         System.out.println("戏说不是胡说，改编不是乱编");
