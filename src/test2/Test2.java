8822a0d967e0b6d0854c8c4593e8bff4046213d2
xb <wojiaoxubo@yeah.net>
2021-02-03 17:33:39 +0800

    22

--- src/test2/Test2.java
+++ src/test2/Test2.java
@@ -3,6 +3,7 @@ package test2;
 public class Test2 {
     public void test2(){
         System.out.println("bxu分支：test2");
-
+        System.out.println(111);
+        System.out.println(222);
     }
 }
